136155adf59e2571e97d7206a311ace9f3890ca2
cheezgi <zackh@firemail.cc>
2016-08-07 22:53:30 +0100

    Add Stone struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 /* Stones esoteric programming language
  * (c) 2016 Zack Hixon - see LICENSE.txt */
 
+#[derive(Debug)]
+//#[derive(Copy, Clone)]
+struct Stone {
+    x: i32,
+    y: i32,
+    id: i32
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut stone = Stone {x: 32, y: 32, id: 1};
+    println!("{:?}", stone);
+
+    stone.x = 64;
+    stone.y = 64;
+    stone.id = 2;
+
+    println!("{:?}", stone);
 }
 
