c06c7d92d66342610b9e209e87575ab6d40961f7
cheezgi <zackh@firemail.cc>
2016-08-07 23:25:47 +0100

    Re-add coordinates to Stone

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,24 @@
 
 #[derive(Debug)]
 struct Stone {
+    x: i32,
+    y: i32,
     id: i32,
     color: String
 }
 
 fn main() {
-    let mut stone = Stone {id: 1, color: String::from("yellow")};
+    let mut stone = Stone {
+        x: 0,
+        y: 0,
+        id: 1,
+        color: String::from("yellow")
+    };
+
     println!("{:?}", stone);
 
+    stone.x = 1;
+    stone.y = 1;
     stone.id = 2;
     stone.color = String::from("blue");
 
