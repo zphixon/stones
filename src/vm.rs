9c3ec294a82d1bb706eb333e0af1a505ead1d3bb
Zack <zphixon@gmail.com>
2022-04-08 18:47:10 -0400

    continue rewrite

--- src/vm.rs
+++ src/vm.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum RedNumber {
     One,
     Two,
@@ -15,7 +15,7 @@ impl RedNumber {
     }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum OrangeNumber {
     One,
     Two,
@@ -30,7 +30,7 @@ impl OrangeNumber {
     }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum OpColor {
     Red(RedNumber),
     Orange(OrangeNumber),
@@ -48,7 +48,7 @@ pub enum Dir {
     Down,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Op {
     color: OpColor,
     dir: Dir,
@@ -75,8 +75,27 @@ impl Op {
     }
 }
 
-#[derive(Default)]
-pub struct Vm;
+#[derive(Default, Debug)]
+pub struct Vm {
+    pub history: Vec<Op>,
+}
+
+impl Vm {
+    fn exec(&mut self, op: Op) {
+        println!("exec {op:?}");
+        self.history.push(op);
+        match op.color() {
+            Stone::Red => {}
+            Stone::Orange => {}
+            Stone::Yellow => {}
+            Stone::Blue => {}
+            Stone::Green => {}
+            Stone::Purple => {}
+
+            _ => unreachable!(),
+        }
+    }
+}
 
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
 pub enum Stone {
@@ -89,9 +108,38 @@ pub enum Stone {
     Purple,
 }
 
-#[derive(Debug)]
+impl Stone {
+    pub fn to_op(&self) -> OpColor {
+        match self {
+            Stone::Red => OpColor::Red(RedNumber::One),
+            Stone::Orange => OpColor::Orange(OrangeNumber::Two),
+            Stone::Yellow => OpColor::Yellow,
+            Stone::Blue => OpColor::Blue,
+            Stone::Green => OpColor::Green,
+            Stone::Purple => OpColor::Purple,
+
+            _ => unreachable!(),
+        }
+    }
+}
+
 pub struct Field<const W: usize = 12, const H: usize = 6> {
     field: [[Stone; W]; H],
+}
+
+impl<const W: usize, const H: usize> std::fmt::Debug for Field<W, H> {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        // ┌┐└┘─│
+        writeln!(formatter, "┌{:─<width$}┐", "─", width = W * 10)?;
+        for row in self.field {
+            write!(formatter, "│")?;
+            for col in row {
+                write!(formatter, "{:10}", format!("{col:?}"))?;
+            }
+            writeln!(formatter, "│")?;
+        }
+        writeln!(formatter, "└{:─<width$}┘", "─", width = W * 10)
+    }
 }
 
 pub struct Wrap {
@@ -125,9 +173,9 @@ pub struct NewPosition {
     pub new_col: usize,
 }
 
-impl<const W: usize, const H: usize> Field<W, H> {
+impl Field<12, 6> {
     #[rustfmt::skip]
-    pub fn new() -> Field<12, 6> {
+    pub fn new() -> Field {
         use Stone::*;
 
         Field {
@@ -141,12 +189,15 @@ impl<const W: usize, const H: usize> Field<W, H> {
             ],
         }
     }
+}
 
+#[allow(non_upper_case_globals)]
+impl<const Width: usize, const Height: usize> Field<Width, Height> {
     fn find(&self, color: Stone) -> (usize, usize) {
         for (row_idx, row) in self.field.iter().enumerate() {
             for (col_idx, my_color) in row.iter().enumerate() {
                 if *my_color == color {
-                    return (row_idx + 1, col_idx + 1);
+                    return (row_idx, col_idx);
                 }
             }
         }
@@ -155,126 +206,179 @@ impl<const W: usize, const H: usize> Field<W, H> {
     }
 
     fn set(&mut self, color: Stone, row: usize, col: usize) {
-        self.field[row - 1][col - 1] = color;
+        self.field[row % Height][col % Width] = color;
     }
 
     fn get(&mut self, row: usize, col: usize) -> Stone {
-        self.field[row - 1][col - 1]
+        self.field[row % Height][col % Width]
     }
 
     pub const fn width(&self) -> usize {
-        W
+        Width
     }
 
     pub const fn height(&self) -> usize {
-        H
+        Height
     }
 
-    pub fn step(&mut self, op: Op, vm: &mut Vm) -> NewPosition {
-        // find the position of the stone we need to move
+    pub fn step(&mut self, vm: &mut Vm, op: Op) {
         let (row_idx, col_idx) = self.find(op.color());
-        // determine how many steps it needs to take
         let mag = op.magnitude();
+        println!("step {op:?}@{row_idx},{col_idx}");
 
-        // determine if that many steps would wrap around the edges
-        let wrap = Wrap {
-            left: col_idx <= mag && op.dir == Dir::Left,
-            right: col_idx + mag > W && op.dir == Dir::Right,
-            up: row_idx <= mag && op.dir == Dir::Up,
-            down: row_idx + mag > H && op.dir == Dir::Down,
-        };
+        for _ in 1..=mag {
+            let new_col = if op.dir == Dir::Left {
+                if col_idx == 0 {
+                    Width - 1
+                } else {
+                    col_idx - 1
+                }
+            } else if op.dir == Dir::Right {
+                col_idx + 1
+            } else {
+                col_idx
+            };
 
-        // calculate the new position
-        let new_col = if wrap.left && op.dir == Dir::Left {
-            W - mag + col_idx
-        } else if wrap.right && op.dir == Dir::Right {
-            W - col_idx - mag
-        } else if op.dir == Dir::Left {
-            col_idx - 1
-        } else if op.dir == Dir::Right {
-            col_idx + 1
-        } else {
-            col_idx
-        };
+            let new_row = if op.dir == Dir::Up {
+                if row_idx == 0 {
+                    Height - 1
+                } else {
+                    row_idx - 1
+                }
+            } else if op.dir == Dir::Down {
+                row_idx + 1
+            } else {
+                row_idx
+            };
+            println!("  -> {new_row},{new_col}");
 
-        let new_row = if wrap.up && op.dir == Dir::Up {
-            H - mag + row_idx
-        } else if wrap.down && op.dir == Dir::Down {
-            H - row_idx - mag
-        } else if op.dir == Dir::Up {
-            row_idx - 1
-        } else if op.dir == Dir::Down {
-            row_idx + 1
-        } else {
-            row_idx
-        };
+            let there = self.get(new_row, new_col);
 
-        // determine if there's anything in the way, and if there is, move it
+            if there != Stone::X {
+                if there < op.color() {
+                    println!("  collide with {there:?}, power through");
+                    self.step(
+                        vm,
+                        Op {
+                            color: there.to_op(),
+                            dir: op.dir,
+                        },
+                    );
+                } else {
+                    println!("  collide with {there:?}, too weak");
+                    break;
+                }
+            }
 
-        match op.color() {
-            Stone::Red => {}
-            Stone::Orange => {}
-            Stone::Yellow => {}
-            Stone::Blue => {}
-            Stone::Green => {}
-            Stone::Purple => {}
-
-            _ => unreachable!(),
+            self.set(op.color(), new_row, new_col);
+            self.set(Stone::X, row_idx, col_idx);
         }
 
-        NewPosition {
-            wrap,
-            new_col,
-            new_row,
-        }
+        vm.exec(op);
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
     #[test]
-    fn step1() {
-        let mut vm = Vm::default();
-        let op = Op {
-            color: OpColor::Red(RedNumber::One),
-            dir: Dir::Left,
-        };
-        let mut field = Field {
-            field: [[Stone::Red, Stone::X]],
-        };
-        let result = field.step(op, &mut vm);
-        assert!(result.wrap.left());
-        assert_eq!(result.new_col, 2);
+    fn cmp() {
+        assert!(Stone::X < Stone::Red);
+        assert!(Stone::Red < Stone::Orange);
+        assert!(Stone::Orange < Stone::Yellow);
+        assert!(Stone::Yellow < Stone::Green);
+        assert!(Stone::Green < Stone::Blue);
+        assert!(Stone::Blue < Stone::Purple);
     }
 
     #[test]
-    fn step2() {
-        let mut vm = Vm::default();
+    fn regular_step() {
         let op = Op {
             color: OpColor::Red(RedNumber::One),
             dir: Dir::Right,
         };
+        let mut vm = Vm::default();
         let mut field = Field {
             field: [[Stone::Red, Stone::X]],
         };
-        let result = field.step(op, &mut vm);
-        assert!(!result.wrap.right());
-        assert_eq!(result.new_col, 2);
+        field.step(&mut vm, op);
+
+        assert_eq!(field.field, [[Stone::X, Stone::Red]]);
+    }
+
+    #[test]
+    fn wrap_left() {
+        let op = Op {
+            color: OpColor::Red(RedNumber::One),
+            dir: Dir::Left,
+        };
+        let mut vm = Vm::default();
+        let mut field = Field {
+            field: [[Stone::Red, Stone::X]],
+        };
+        field.step(&mut vm, op);
+
+        assert_eq!(field.field, [[Stone::X, Stone::Red]]);
+    }
+
+    #[test]
+    fn wrap_right() {
+        let op = Op {
+            color: OpColor::Red(RedNumber::Two),
+            dir: Dir::Left,
+        };
+        let mut vm = Vm::default();
+        let mut field = Field {
+            field: [[Stone::Red, Stone::X]],
+        };
+        field.step(&mut vm, op);
+
+        assert_eq!(field.field, [[Stone::Red, Stone::X]]);
+    }
+
+    #[test]
+    fn step2() {
+        let op = Op {
+            color: OpColor::Blue,
+            dir: Dir::Right,
+        };
+        let mut vm = Vm::default();
+        let mut field = Field {
+            field: [[Stone::Blue, Stone::Red, Stone::X]],
+        };
+        field.step(&mut vm, op);
+
+        assert_eq!(field.field, [[Stone::X, Stone::Blue, Stone::Red]]);
+        assert_eq!(
+            vm.history,
+            vec![
+                Op {
+                    color: OpColor::Red(RedNumber::One),
+                    dir: Dir::Right,
+                },
+                Op {
+                    color: OpColor::Blue,
+                    dir: Dir::Right,
+                }
+            ]
+        );
     }
 
     #[test]
     fn step3() {
-        let mut vm = Vm::default();
         let op = Op {
-            color: OpColor::Blue,
-            dir: Dir::Up,
+            color: OpColor::Red(RedNumber::Two),
+            dir: Dir::Left,
         };
+        let mut vm = Vm::default();
         let mut field = Field {
-            field: [[Stone::Blue, Stone::X]],
+            field: [[Stone::Red, Stone::X, Stone::X, Stone::Blue, Stone::X]],
         };
-        let result = field.step(op, &mut vm);
-        assert!(result.wrap.up());
-        assert_eq!(result.new_row, 1);
+        println!("{field:?}");
+        field.step(&mut vm, op);
+        println!("{field:?}");
+
+        panic!("{vm:?}");
     }
 }
