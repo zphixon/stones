cdd043d4169ec24ad41d28658d99a6704316d257
Zack <zphixon@gmail.com>
2022-04-12 12:24:40 -0400

    polish cli a lil

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,27 @@ struct Args {
     filename: Option<String>,
 
     #[options(
-        help = "If no filename is present, show the tokens scanned from each line of REPL input, or if given a filename show the tokens scanned from the file and exit.",
+        help = "If no filename is present, show the tokens scanned from the file or each line of REPL input.",
         short = "t"
     )]
     print_tokens: bool,
 
     #[options(
-        help = "If no filename is present, show the AST parsed from each line of REPL input, or if given a filename show the AST parsed from the file and exit.",
+        help = "If no filename is present, show the AST parsed from the file or each line of REPL input.",
         short = "a"
     )]
     print_ast: bool,
 
     #[options(
-        help = "If no filename is present, show the operations compiled from the AST from each line of REPL input, or if given a filename show the operations compiled from the file and exit.",
+        help = "Show compiled bytecode from the file or each line of REPL input.",
         short = "c"
     )]
     print_compiled: bool,
 
-    #[options(help = "Quit after parsing and compiling a file.", short = "v")]
+    #[options(
+        help = "Quit after parsing and compiling a file. Requires filename.",
+        short = "v"
+    )]
     verify_syntax: bool,
 
     #[options(help = "Print the operation being executed.", short = "o")]
@@ -51,29 +54,40 @@ struct Args {
     interactive: bool,
 }
 
+impl Args {
+    fn print_any(&self) -> bool {
+        self.print_tokens
+            || self.print_ast
+            || self.print_compiled
+            || self.print_operation
+            || self.print_field
+            || self.print_stack
+    }
+}
+
 fn main() {
     let mut args: Args = Args::parse_args_default_or_exit();
-
-    // TODO handle more combinations
     args.print_operation = args.print_operation || args.print_field || args.print_stack;
 
     if args.filename.is_none() || args.interactive {
-        todo!();
+        todo!("repl not supported yet");
     }
 
     let source = std::fs::read_to_string(args.filename.as_ref().unwrap()).unwrap();
-    if args.print_tokens {
-        println!("{:#?}", stones::scan(&source).collect::<Vec<_>>());
-    }
-
     let ast = stones::parse(&source).unwrap();
-    if args.print_ast {
-        println!("{ast:#?}");
-    }
-
     let program = stones::compile(&ast);
+
+    if args.print_tokens {
+        println!("tokens:\n{:#?}", stones::scan(&source).collect::<Vec<_>>());
+    }
+    if args.print_ast {
+        println!("ast:\n{ast:#?}");
+    }
     if args.print_compiled {
-        println!("{program:#?}");
+        println!("bytecode:");
+        for (i, stones::vm::Operation { command, opcode }) in program.iter().enumerate() {
+            stones::print_command_opcode(i, command, *opcode);
+        }
     }
 
     if args.filename.is_some() && args.verify_syntax {
@@ -81,6 +95,10 @@ fn main() {
     }
 
     let mut vm = stones::vm::Vm::new(program);
+
+    if args.print_any() {
+        println!("program run:");
+    }
 
     if args.print_field {
         println!("init\n{:?}\n", vm.field());
